5d0d5894e85b3e00b479f82bf158b1b3d8ea4b52
Nathan Meldrum <126224887+LogicalMagician@users.noreply.github.com>
2023-02-27 05:50:35 +0000

    Update steg.bat

--- steg.bat
+++ steg.bat
@@ -13,13 +13,15 @@ if "%mode%"=="hide" (
     echo Enter the name of the text file to hide:
     set /p text=
 
-    :: Read the text file and encode it in base64
-    set /p secret=<%text%
-    set encoded=
-    for /f "tokens=3 delims= " %%a in ('certutil -encode "!text!" temp.b64 ^| findstr /i /v /c:"- "') do set encoded=!encoded!%%a
-    set encoded=!encoded:~0,-2!
+    :: Read the text file and encrypt it using AES
+    echo Enter a password to encrypt the text:
+    set /p password=
+    powershell -Command "$Key=(1..32)|%{[char][byte](Get-Random(0,256))};$IV=(1..16)|%{[char][byte](Get-Random(0,256))};$Key=[Convert]::ToBase64String($Key);$IV=[Convert]::ToBase64String($IV);$Key | Out-File temp.key;$IV | Out-File temp.iv;$secret = Get-Content !text!; $secret | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString -Key $Key -IV $IV | Out-File secret.enc -Encoding ASCII"
+    set /p key=<temp.key
+    set /p iv=<temp.iv
+    set /p secret=<secret.enc
 
-    :: Hide the encoded text in the image
+    :: Hide the encrypted text in the image
     echo Enter a password to secure the image:
     set /p password=
     echo Hiding text inside the image...
@@ -46,14 +48,10 @@ if "%mode%"=="extract" (
     copy /b !output!+secret.b64 secret2.b64
     set /p secret2=<secret2.b64
     set decoded=
-    for /f "tokens=3 delims= " %%a in ('certutil -decode secret2.b64 temp.txt ^| findstr /i /v /c:"- "') do set decoded=!decoded!%%a
-    set decoded=!decoded:~0,-2!
+    powershell -Command "$Key=[Convert]::FromBase64String((Get-Content temp.key));$IV=[Convert]::FromBase64String((Get-Content temp.iv));$secret=[System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR((ConvertTo-SecureString (Get-Content secret2.b64) -Key $Key -IV $IV)));Remove-Item temp.key,temp.iv,secret2.b64;echo $secret" > temp.txt
+    set /p decoded=<temp.txt
     echo !decoded!>%output%
 
     echo Text extracted from the image.
 )
 
-:: Clean up temporary files
-del temp.b64 secret.b64 secret2.b64 temp.txt
-
-endlocal
